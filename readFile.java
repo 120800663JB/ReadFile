7ba86b88e321431d409feb1405b31d74e53b1bbb
120800663JB <112689486+120800663JB@users.noreply.github.com>
2023-03-03 10:04:37 +0000

    Create readFile.java

---
+++ readFile.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+import java.io.*;
+
+class runner {
+    int bibNo;
+    String name;
+    int age;
+    int laneNo;
+    double time;
+}
+
+
+public class runners {
+    static Scanner keyboard = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+
+        // main class stuff
+        
+        // setup runners array
+        runner [] runnerDetails = new runner[8];
+
+        // read file
+        readFile(runnerDetails);
+        
+    }
+
+    // read file sb program
+    public static void readFile(runner [] runners) throws IOException {
+        
+        // change file name
+        String fileName = "Runners.csv";
+        
+        Scanner fileScanner = new Scanner (new BufferedReader (new FileReader(fileName)));
+        
+        fileScanner.useDelimiter("[\\r\\n,]+");
+        
+        
+
+        try{
+            fileScanner = new Scanner (new BufferedReader (new FileReader(fileName)));
+            fileScanner.useDelimiter("[\\r\\n,]+");
+            int currentLine = 0;
+            while (fileScanner.hasNext()){
+                runners[currentLine] = new runner();
+                runners[currentLine].bibNo = fileScanner.nextInt();
+                runners[currentLine].name = fileScanner.next();
+                runners[currentLine].age = fileScanner.nextInt();
+                runners[currentLine].laneNo = fileScanner.nextInt();
+                runners[currentLine].time = fileScanner.nextDouble();
+                currentLine = currentLine + 1;
+            }
+        }
+        catch (FileNotFoundException error) {
+            System.out.println("file not found " + error);
+        }
+
+        finally {
+            if (fileScanner != null) {
+                fileScanner.close();
+            }
+        }
+    }
+}
